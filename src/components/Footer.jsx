0e397d69ffcb3402045da608048864ff28ab4088
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2025-11-25 00:23:42 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
+import { Github } from 'lucide-react';
+
 export default function Footer() {
   return (
     <footer className="flex justify-between bg-gray-800 text-gray-400 text-center p-4 mt-6">
       <p>© {new Date().getFullYear()} Emprog. All rights reserved.</p>
+      <a
+        href="https://github.com/ShejulShubham/Emprog"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Github />
+      </a>
       <p>Version 1.25.11</p>
     </footer>
   );
