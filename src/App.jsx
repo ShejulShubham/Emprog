b4e729179b8540214d26780209b638c86616cc29
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2026-01-18 19:03:04 +0530

    added sorting option

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./pages/Dashboard";
 import RedirectIfNeeded from "./components/RouteGuard";
 import NotFound from "./pages/NotFound";
 import MainLayout from "./layout/MainLayout";
+import UserSettings from "./pages/UserSettings";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/auth" element={<Auth isRegistration={true} />} />
             <Route path="/login" element={<Auth />} />
+            <Route path="/settings" element={<UserSettings />} />
           </Route>
 
           {/* GROUP 2: Standalone Pages */}
